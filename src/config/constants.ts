ccdfe78cd8588ba1d2a56f02d016f2f403f76175
Meyoron Aghogho <youngmayor.dev@gmail.com>
2025-06-03 01:01:51 +0000

    config update

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,11 +2,9 @@
 // @/src/config/constants.ts
 export const PROJECT_NAME = "ShellBase";
 export const PROJECT_DESCRIPTION = "ShellBase: Your one-stop cheatsheet API for command-line power tools.";
-export const PROJECT_URL = "https://your-app-url.com"; // TODO: Replace with actual URL if available
+export const PROJECT_URL = "https://shellbase.netlify.app"; // TODO: Replace with actual URL if available
 export const PROJECT_AUTHOR = "MayR Labs";
-export const PROJECT_AUTHOR_GITHUB_USERNAME = "youngmayor";
+export const PROJECT_AUTHOR_GITHUB_USERNAME = "YoungMayor";
 export const PROJECT_AUTHOR_LINK = `https://github.com/${PROJECT_AUTHOR_GITHUB_USERNAME}`;
 export const PROJECT_GITHUB_LINK = `https://github.com/${PROJECT_AUTHOR_GITHUB_USERNAME}/shellbase-api-nextjs`; 
 export const API_DOCS_URL = "/api-docs";
-
-    
